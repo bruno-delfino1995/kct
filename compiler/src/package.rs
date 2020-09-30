a9a9c77cc48b021c4f585b4299efd22c8e7f64b4
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2020-09-29 21:18:44 -0300

    Render KCP from .tgz
    Now we're able to render configuration packages that were compressed
    with `tar czf kcp.tgz -C kcp .`, and maybe published to a centralized
    repository of kubernetes configuration. Thus the operator doesn't need
    to unpack the compressed package into a directory to later compile it,
    we'll do all on our own using temporary directories. Although, as we're
    relying on temporary directories we've also chosen to rely on the
    operating system to clean them.
    
    Close: #8

--- compiler/src/package.rs
+++ compiler/src/package.rs
@@ -5,7 +5,11 @@ pub mod spec;
 use self::error::{Error, Result};
 use self::schema::Schema;
 use self::spec::Spec;
+use flate2::read::GzDecoder;
+use std::fs::File;
 use std::path::PathBuf;
+use tar::Archive;
+use tempfile::TempDir;
 
 const SCHEMA_FILE: &str = "values.schema.json";
 const SPEC_FILE: &str = "kcp.json";
@@ -18,6 +22,11 @@ pub struct Package {
 
 impl Package {
 	pub fn from_path(root: PathBuf) -> Result<Self> {
+		let root = match root.extension() {
+			None => root,
+			Some(_) => decompress(root)?,
+		};
+
 		let mut spec = root.clone();
 		spec.push(SPEC_FILE);
 		let spec = Spec::from_path(spec)?;
@@ -32,4 +41,24 @@ impl Package {
 
 		Ok(Package { root, spec, schema })
 	}
+}
+
+fn decompress(archive: PathBuf) -> Result<PathBuf> {
+	let ext = archive.extension().unwrap().to_str();
+	if ext != Some("tgz") {
+		return Err(Error::InvalidFormat);
+	}
+
+	let dir = TempDir::new()
+		.expect("Unable to create temporary directory to unpack your KCP")
+		.into_path();
+
+	let kcp = File::open(archive).expect("Unable to read KCP archive");
+	let tar = GzDecoder::new(kcp);
+	let mut archive = Archive::new(tar);
+	archive
+		.unpack(dir.to_str().unwrap())
+		.expect("Unable to extract your KCP archive");
+
+	Ok(dir)
 }
