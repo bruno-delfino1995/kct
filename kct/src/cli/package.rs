43842e657d4ca7ad244a24bbf57c3c61568d1b5b
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2020-09-29 22:33:33 -0300

    Add package command to convert a KCP into a .tgz
    We shouldn't rely on the user for packaging the KCP into a format that
    we define. Thus, the process previously expected from the user is now
    done with the `package` command.
    
    This command expects a valid KCP directory to create an archive on the
    current working directory with the name specified at `kcp.json`.

---
+++ kct/src/cli/package.rs
@@ -0,0 +1,30 @@
+use clap::ArgMatches;
+use clap::{App, Arg, SubCommand};
+use compiler::Package;
+use std::path::PathBuf;
+
+pub fn command() -> App<'static, 'static> {
+	SubCommand::with_name("package")
+		.about("package a KCP into a KCP Archive")
+		.arg(
+			Arg::with_name("package")
+				.help("Directory containing the KCP")
+				.index(1)
+				.required(true),
+		)
+}
+
+// TODO: Can't we use Box<dyn Display> for error since all our errors implement
+// Display?
+pub fn run(matches: &ArgMatches) -> Result<String, String> {
+	let package_from: PathBuf = matches.value_of("package").map(PathBuf::from).unwrap();
+	let package = Package::from_path(package_from).map_err(|err| err.to_string())?;
+
+	let cwd = std::env::current_dir().map_err(|err| err.to_string())?;
+	let compressed_path = package.compress(&cwd)?;
+
+	Ok(format!(
+		"Successfully packaged KCP and saved it to: {}",
+		compressed_path.to_str().unwrap()
+	))
+}
