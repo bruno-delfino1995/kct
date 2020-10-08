ffd50ed559dccaf63b9a5d0d496a21f73a82309c
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2020-10-08 00:19:43 -0300

    Add release flag to allow multiple applies
    To support deploying the same set of objects more than once, we're
    introducing the release parameter that'll prepend your package name with
    the release name under `package.fullName` within the global. This naming
    and prepending align with Helm's convention of giving resources names
    that contain your release name, followed by the chart name.
    
    Close: #7

--- kct/src/cli/compile.rs
+++ kct/src/cli/compile.rs
@@ -1,7 +1,7 @@
 use clap::ArgMatches;
 use clap::{App, Arg, SubCommand};
 use helper::io;
-use package::Package;
+use package::{Package, Release};
 use serde_json::Value;
 use std::path::PathBuf;
 
@@ -21,6 +21,12 @@ pub fn command() -> App<'static, 'static> {
 				.help("Sets the values for the package")
 				.takes_value(true),
 		)
+		.arg(
+			Arg::with_name("release")
+				.long("release")
+				.help("Scope your package within a release")
+				.takes_value(true),
+		)
 }
 
 // TODO: Can't we use Box<dyn Display> for error since all our errors implement
@@ -32,7 +38,13 @@ pub fn run(matches: &ArgMatches) -> Result<String, String> {
 	let package_from: PathBuf = matches.value_of("package").map(PathBuf::from).unwrap();
 	let package = Package::from_path(package_from).map_err(|err| err.to_string())?;
 
-	let rendered = package.compile(values).map_err(|err| err.to_string())?;
+	let release = matches.value_of("release").map(|name| Release {
+		name: String::from(name),
+	});
+
+	let rendered = package
+		.compile(values, release)
+		.map_err(|err| err.to_string())?;
 
 	let objects = kube::find(&rendered).map_err(|err| err.to_string())?;
 	let to_apply = kube::glue(&objects);
