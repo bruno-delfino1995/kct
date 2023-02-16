192d977c5d6a174cfa1029330162f883ba40da5e
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2023-02-15 22:42:08 -0300

    Use traits for apply and delete of objects

--- crates/kct_kube/src/client.rs
+++ crates/kct_kube/src/client.rs
@@ -5,6 +5,7 @@ pub use crate::error::Root as Error;
 use std::time::Duration;
 
 use anyhow::Result;
+use async_trait::async_trait;
 use either::Either;
 use futures::TryFutureExt;
 use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinition as CRD;
@@ -12,19 +13,22 @@ use kube::api::{Api, DynamicObject as Dynamic, Patch, PatchParams, ResourceExt};
 use kube::core::GroupVersionKind;
 use kube::discovery::{Discovery, Scope};
 use kube::runtime::wait::{await_condition, conditions};
-use kube::Client as K8;
+use kube::Client as K8s;
 
 pub struct Client {
-	client: K8,
+	internal: K8s,
 	discovery: Discovery,
 }
 
 impl Client {
 	pub async fn try_new() -> Result<Self> {
-		let client = K8::try_default().await?;
-		let discovery = Discovery::new(client.clone()).run().await?;
+		let internal = K8s::try_default().await?;
+		let discovery = Discovery::new(internal.clone()).run().await?;
 
-		Ok(Self { client, discovery })
+		Ok(Self {
+			internal,
+			discovery,
+		})
 	}
 
 	pub async fn apply(&mut self, manifests: Vec<Manifest>) -> Result<()> {
@@ -34,7 +38,7 @@ impl Client {
 		let crds = plan
 			.crds
 			.into_iter()
-			.map(|crd| self.apply_crd(crd, &ssapply));
+			.map(|crd| crd.apply(self, &ssapply).inspect_ok(Client::applied));
 		let _ = futures::future::try_join_all(crds).await?;
 
 		self.refresh().await?;
@@ -42,46 +46,14 @@ impl Client {
 		let dynamics = plan
 			.dynamics
 			.into_iter()
-			.map(|obj| self.apply_dynamic(obj, &ssapply));
+			.map(|obj| obj.apply(self, &ssapply).inspect_ok(Client::applied));
 		let _ = futures::future::try_join_all(dynamics).await?;
 
 		Ok(())
 	}
 
-	async fn apply_crd(&self, tracked: Tracked<CRD>, params: &PatchParams) -> Result<()> {
-		let name = tracked.value().name_any();
-		let patch = Patch::Apply(tracked.value());
-		let cond = conditions::is_crd_established();
-
-		let api: Api<CRD> = Api::all(self.client.clone());
-		let apply = api
-			.patch(&name, params, &patch)
-			.map_err(|err| anyhow::anyhow!(err));
-
-		let api: Api<CRD> = Api::all(self.client.clone());
-		let wait = {
-			let establish = await_condition(api, &name, cond).map_err(|err| anyhow::anyhow!(err));
-
-			tokio::time::timeout(Duration::from_secs(10), establish)
-				.map_err(|err| anyhow::anyhow!(err))
-		};
-
-		let _ = futures::future::try_join(apply, wait).await?;
-
-		println!("{} created", tracked.path().display());
-
-		Ok(())
-	}
-
-	async fn apply_dynamic(&self, tracked: Tracked<Dynamic>, params: &PatchParams) -> Result<()> {
-		let name = tracked.value().name_any();
-		let api: Api<Dynamic> = self.dynamic_api(tracked.value())?;
-		let data = serde_json::to_value(tracked.value())?;
-		let _ = api.patch(&name, params, &Patch::Apply(data)).await?;
-
-		println!("{} created", tracked.path().display());
-
-		Ok(())
+	fn applied(path: &String) {
+		println!("{path} created")
 	}
 
 	pub async fn delete(&mut self, mut manifests: Vec<Manifest>) -> Result<()> {
@@ -92,8 +64,11 @@ impl Client {
 		let dynamics = plan
 			.dynamics
 			.into_iter()
-			.map(|obj| self.delete_dynamic(obj));
-		let crds = plan.crds.into_iter().map(|obj| self.delete_crd(obj));
+			.map(|obj| obj.delete(self).inspect_ok(Client::deleted));
+		let crds = plan
+			.crds
+			.into_iter()
+			.map(|obj| obj.delete(self).inspect_ok(Client::deleted));
 
 		let _ = futures::future::try_join_all(dynamics).await?;
 		let _ = futures::future::try_join_all(crds).await?;
@@ -101,57 +76,14 @@ impl Client {
 		Ok(())
 	}
 
-	async fn delete_crd(&self, crd: Tracked<CRD>) -> Result<()> {
-		let name = crd.value().name_any();
-		let api: Api<CRD> = Api::all(self.client.clone());
-		let _ = api.delete(&name, &Default::default()).await?;
-
-		println!("{} removed", crd.path().display());
-
-		Ok(())
-	}
-
-	async fn delete_dynamic(&self, dynamic: Tracked<Dynamic>) -> Result<()> {
-		let name = dynamic.value().name_any();
-		let api: Api<Dynamic> = self.dynamic_api(dynamic.value())?;
-		let _ = api.delete(&name, &Default::default()).await?;
-
-		println!("{} removed", dynamic.path().display());
-
-		Ok(())
+	fn deleted(path: &String) {
+		println!("{path} deleted")
 	}
 
 	async fn refresh(&mut self) -> Result<()> {
-		self.discovery = Discovery::new(self.client.clone()).run().await?;
+		self.discovery = Discovery::new(self.internal.clone()).run().await?;
 
 		Ok(())
-	}
-
-	fn dynamic_api(&self, obj: &Dynamic) -> Result<Api<Dynamic>> {
-		let gvk = obj
-			.types
-			.as_ref()
-			.ok_or(anyhow::anyhow!(
-				"cannot apply object without valid TypeMeta {:?}",
-				obj
-			))
-			.and_then(|tm| {
-				let gvk = GroupVersionKind::try_from(tm)?;
-
-				Ok(gvk)
-			})?;
-
-		let (ar, caps) = self.discovery.resolve_gvk(&gvk).ok_or(anyhow::anyhow!(
-			"unable to resolve resource and capabilities from dynamic object {:?}",
-			obj
-		))?;
-
-		let client = self.client.clone();
-		if caps.scope == Scope::Cluster {
-			Ok(Api::all_with(client, &ar))
-		} else {
-			Ok(Api::default_namespaced_with(client, &ar))
-		}
 	}
 }
 
@@ -182,5 +114,108 @@ fn try_crd(obj: Dynamic) -> Either<Dynamic, CRD> {
 	match obj.clone().try_parse() {
 		Ok(crd) => Either::Right(crd),
 		Err(_) => Either::Left(obj),
+	}
+}
+
+#[async_trait]
+trait Object {
+	type Kind;
+
+	async fn apply(self, client: &Client, params: &PatchParams) -> Result<String>;
+
+	async fn delete(self, client: &Client) -> Result<String>;
+
+	fn api(&self, client: &Client) -> Result<Api<Self::Kind>>;
+}
+
+#[async_trait]
+impl Object for Tracked<Dynamic> {
+	type Kind = Dynamic;
+
+	async fn apply(self, client: &Client, params: &PatchParams) -> Result<String> {
+		let value = self.value();
+		let name = value.name_any();
+
+		let api = self.api(client)?;
+		let data = serde_json::to_value(value)?;
+		let _ = api.patch(&name, params, &Patch::Apply(data)).await?;
+
+		Ok(format!("{}", self.path().display()))
+	}
+
+	async fn delete(self, client: &Client) -> Result<String> {
+		let name = self.value().name_any();
+		let api = self.api(client)?;
+		let _ = api.delete(&name, &Default::default()).await?;
+
+		Ok(format!("{}", self.path().display()))
+	}
+
+	fn api(&self, client: &Client) -> Result<Api<Self::Kind>> {
+		let obj = self.value();
+		let gvk = obj
+			.types
+			.as_ref()
+			.ok_or(anyhow::anyhow!(
+				"cannot apply object without valid TypeMeta {:?}",
+				obj
+			))
+			.and_then(|tm| {
+				let gvk = GroupVersionKind::try_from(tm)?;
+
+				Ok(gvk)
+			})?;
+
+		let (ar, caps) = client.discovery.resolve_gvk(&gvk).ok_or(anyhow::anyhow!(
+			"unable to resolve resource and capabilities from dynamic object {:?}",
+			obj
+		))?;
+
+		let client = client.internal.clone();
+		if caps.scope == Scope::Cluster {
+			Ok(Api::all_with(client, &ar))
+		} else {
+			Ok(Api::default_namespaced_with(client, &ar))
+		}
+	}
+}
+
+#[async_trait]
+impl Object for Tracked<CRD> {
+	type Kind = CRD;
+
+	async fn apply(self, client: &Client, params: &PatchParams) -> Result<String> {
+		let name = self.value().name_any();
+		let patch = Patch::Apply(self.value());
+		let cond = conditions::is_crd_established();
+
+		let api = self.api(client)?;
+		let apply = api
+			.patch(&name, params, &patch)
+			.map_err(|err| anyhow::anyhow!(err));
+
+		let api = self.api(client)?;
+		let wait = {
+			let establish = await_condition(api, &name, cond).map_err(|err| anyhow::anyhow!(err));
+
+			tokio::time::timeout(Duration::from_secs(10), establish)
+				.map_err(|err| anyhow::anyhow!(err))
+		};
+
+		let _ = futures::future::try_join(apply, wait).await?;
+
+		Ok(format!("{}", self.path().display()))
+	}
+
+	async fn delete(self, client: &Client) -> Result<String> {
+		let name = self.value().name_any();
+		let api = self.api(client)?;
+		let _ = api.delete(&name, &Default::default()).await?;
+
+		Ok(format!("{}", self.path().display()))
+	}
+
+	fn api(&self, client: &Client) -> Result<Api<Self::Kind>> {
+		Ok(Api::all(client.internal.clone()))
 	}
 }
